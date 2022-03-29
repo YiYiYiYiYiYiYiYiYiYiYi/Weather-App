1580400796c487b2797996bf02fdf403775ded61
YiYiYiYiYiYiYiYiYiYiYi <101046167+YiYiYiYiYiYiYiYiYiYiYi@users.noreply.github.com>
2022-03-29 20:36:45 +0200

    showing the current temperature

---
+++ src/app.js
@@ -0,0 +1,18 @@
+function displayTemperature(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = response.data.name;
+  let descriptionElement = document.querySelector("#description");
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  let humidityElement = document.querySelector("#humidity");
+  humidityElement.innerHTML = response.data.main.humidity;
+  let windElement = document.querySelector("#wind");
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+}
+
+let apiKey = "17881ef8c7b51a3f397dbb41040ad179";
+let city = "New York";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
+axios.get(apiUrl).then(displayTemperature);
