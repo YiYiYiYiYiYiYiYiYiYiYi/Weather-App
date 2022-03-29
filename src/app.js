3af37aee24bc28757359e7a0654376b52d29c8e4
YiYiYiYiYiYiYiYiYiYiYi <101046167+YiYiYiYiYiYiYiYiYiYiYi@users.noreply.github.com>
2022-03-29 20:53:13 +0200

    updated the time

--- src/app.js
+++ src/app.js
@@ -1,3 +1,27 @@
+function formateDate() {
+  let date = new Date();
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  return `${day} ${hours}:${minutes}`;
+}
+
 function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
@@ -9,6 +33,8 @@ function displayTemperature(response) {
   humidityElement.innerHTML = response.data.main.humidity;
   let windElement = document.querySelector("#wind");
   windElement.innerHTML = Math.round(response.data.wind.speed);
+  let dateElement = document.querySelector("#date");
+  dateElement.innerHTML = formateDate();
 }
 
 let apiKey = "17881ef8c7b51a3f397dbb41040ad179";
