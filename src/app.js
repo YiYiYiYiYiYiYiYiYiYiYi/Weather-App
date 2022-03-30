537136fb85f5dd1635846775c519dce9c1f96b17
YiYiYiYiYiYiYiYiYiYiYi <101046167+YiYiYiYiYiYiYiYiYiYiYi@users.noreply.github.com>
2022-03-30 22:58:19 +0200

    added search engine

--- src/app.js
+++ src/app.js
@@ -42,8 +42,19 @@ function displayTemperature(response) {
   );
 }
 
-let apiKey = "17881ef8c7b51a3f397dbb41040ad179";
-let city = "New York";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+function search(city) {
+  let apiKey = "17881ef8c7b51a3f397dbb41040ad179";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayTemperature);
+}
 
-axios.get(apiUrl).then(displayTemperature);
+function handleSubmit(event) {
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#city-input");
+  search(cityInputElement.value);
+}
+
+search("New York");
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
